e5a27c8b7de24bd1b09e928cf634f5a4eded304f
Alex-HP <aleksandr.lozinskiy2005@gmail.com>
2023-10-21 19:32:19 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
+import styles from './App.module.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const actionSigns = [
+        ['+', clickNumber],
+        ['-', clickNumber],
+        ['=', getResult],
+        ['C', reset]
+    ];
+
+    const numbersBtns = numbers.map(item => {
+        return <button onClick={clickNumber} key={item} className={styles.btnNumber}>{item}</button>;
+    });
+
+    const actionsBtns = actionSigns.map(item => {
+        return <button key={item[0]} onClick={item[1]} className={styles.actionItem}>{item[0]}</button>;
+    });
+
+    const [elements, setElements] = useState('');
+    const [action, setAction] = useState('');
+    const [screenClass, setScreenClass] = useState('screen');
+
+    function clickNumber(event) {
+        setElements(elements + event.target.innerHTML);
+        if (event.target.innerHTML === '+') {
+            setAction('+');
+        }
+        if (event.target.innerHTML === '-') {
+            setAction('-');
+        }
+
+        setScreenClass('screen');
+    }
+    
+    function reset() {
+        setElements('');
+    }
+
+    function getResult() {
+        const [num1, num2] = elements.split(action);
+        
+        if (action === '+') {
+            setElements(+num1 + +num2);
+        } else {
+            setElements(+num1 - +num2);
+        }
+
+        setScreenClass('screenGreen');
+    }
+
+    return (
+        <div className={styles.app}>
+            <header className={styles.appHeader}>
+                <div className={screenClass === 'screen' ? styles.screen : styles.screenGreen}>{elements}</div>
+                <div className={styles.btnWrapper}>
+                    {numbersBtns}
+                </div>
+                <div className={styles.actionsWrapper}>
+                    {actionsBtns}
+                </div>
+            </header>
+        </div>
+    );
 }
 
 export default App;
